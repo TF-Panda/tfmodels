95a48287f5d69db494116f6f463344bf530789e1
Brian Lach <brianlach72@gmail.com>
2021-05-17 16:37:02 -0400

    audio

---
+++ src/audio/bgm/Sources.pp
@@ -0,0 +1,11 @@
+#define DIR_TYPE models
+#define INSTALL_TO audio/bgm
+
+#begin install_audio
+  #define SOURCES \
+    gamestartup1.mp3 gamestartup2.mp3 gamestartup3.mp3 \
+    gamestartup4.mp3 gamestartup5.mp3 gamestartup6.mp3 \
+    gamestartup7.mp3 gamestartup8.mp3 gamestartup9.mp3 \
+    gamestartup10.mp3 gamestartup11.mp3 gamestartup12.mp3 \
+    gamestartup13.mp3 gamestartup14.mp3
+#end install_audio
---
+++ src/audio/sfx/Sources.pp
@@ -0,0 +1,7 @@
+#define DIR_TYPE models
+#define INSTALL_TO audio/sfx
+
+#begin install_audio
+  #define SOURCES \
+    buttonclick.wav buttonclickrelease.wav buttonrollover.wav
+#end install_audio

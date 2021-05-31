b23d9cc9378b840cf7ba08cd84b61042917450cf
Brian Lach <brianlach72@gmail.com>
2021-05-31 13:21:46 -0400

    more audio

--- src/audio/sfx/Sources.pp
+++ src/audio/sfx/Sources.pp
@@ -3,5 +3,9 @@
 
 #begin install_audio
   #define SOURCES \
-    buttonclick.wav buttonclickrelease.wav buttonrollover.wav
+    buttonclick.wav buttonclickrelease.wav buttonrollover.wav \
+    wpn_moveselect.ogg \
+    shotgun_cock_back.wav shotgun_cock_forward.wav shotgun_shoot.wav \
+    draw_secondary.wav draw_primary.wav draw_wrench_engineer.wav \
+    metal_hit_hand1.wav metal_hit_hand2.wav metal_hit_hand3.wav
 #end install_audio
